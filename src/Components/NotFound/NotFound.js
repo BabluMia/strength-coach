69e1cf71b0e201103b9756a1ed3b67f6a34b8d1d
Bablu Mia <bablumiah2000@gmail.com>
2022-04-18 00:54:04 +0600

    final add

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
     return (
         <div className='error mt-5 py-5  ps-0 ps-lg-5'>
             <div className='error-div ps-0 ps-lg-5'>
-               <img src="https://i.postimg.cc/PxM0wH8X/7aqcppklh6bexoa70320.webp" alt="" />
+               <img src="https://cdn.dribbble.com/users/1022481/screenshots/3018253/404-snow.gif" alt="" />
             </div>
         </div>
     );
